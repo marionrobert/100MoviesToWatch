7bbc054fd382aa7d975eecdc6ddec5c4e2f4d79e
Marion <mrobert148@gmail.com>
2023-03-14 11:50:35 +0100

    adding this line response.encoding = response.apparent_encoding

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/m
 
 # Write your code below this line 👇
 response = requests.get(URL)
+response.encoding = response.apparent_encoding
 web_text = response.text
 # print(web_text)
 
