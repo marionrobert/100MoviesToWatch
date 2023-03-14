69cb3a65db2f833d36d836100d50f73f9bc9b9ec
Marion <mrobert148@gmail.com>
2023-03-14 11:40:21 +0100

    web scrapping the list of all 100 movies from an archive of empireonline website

--- main.py
+++ main.py
@@ -4,5 +4,12 @@ from bs4 import BeautifulSoup
 URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
 # Write your code below this line 👇
+response = requests.get(URL)
+web_text = response.text
+# print(web_text)
 
+soup = BeautifulSoup(web_text, "html.parser")
+# print(soup.prettify())
 
+all_movies_titles = [movie.getText() for movie in soup.find_all(name="h3", class_="title")]
+all_movies_titles.reverse()
