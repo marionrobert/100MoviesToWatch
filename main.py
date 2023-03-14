8a57eeebda15e458285e0ad85e197d79c3eb99d4
Marion <mrobert148@gmail.com>
2023-03-14 11:58:34 +0100

    another way to resolve encoding error: use repsonse.content istead of response.text

--- main.py
+++ main.py
@@ -5,11 +5,18 @@ URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/m
 
 # Write your code below this line 👇
 response = requests.get(URL)
+
+#TODO: to avoid encoding text
+
+# first_solution :
 # print(response.encoding) --> "ISO-8859-1"
 # print(response.apparent_encoding) --> "utf-8"
-response.encoding = response.apparent_encoding
-web_text = response.text
-# print(web_text)
+# response.encoding = response.apparent_encoding
+# web_text = response.text
+
+# second_solution:
+# After getting response, take response.content instead of response.text and that will be of encoding utf-8.
+web_text = response.content
 
 soup = BeautifulSoup(web_text, "html.parser")
 # print(soup.prettify())
